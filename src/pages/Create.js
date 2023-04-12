11a7f72f86a267ab2a265cdd6a05cc8f141c3f55
mantaslv <mantasvolkas@gmail.com>
2023-04-12 14:42:52 +0100

    Create a new note page added with h2 and button

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Typography, Button, Container } from '@material-ui/core'
 
 export default function Create() {
   return (
-    <div>
-      Create page
-    </div>
+    <Container>
+      <Typography
+        variant='h6'
+        component='h2'
+        gutterBottom
+        color='textSecondary'
+      >
+        Create a New Note
+      </Typography>
+
+      <Button
+        onClick={() => console.log('you clicked me')}
+        type='submit'
+        color='secondary'
+        variant='contained'
+      >
+        Submit
+      </Button>
+
+      {/* <Button type='submit'>Submit</Button>
+      <Button type='submit' color='secondary' variant='outlined'>Submit</Button> */}
+
+      {/* <ButtonGroup color='secondary' variant='contained'>
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+    </Container>
   )
 }
