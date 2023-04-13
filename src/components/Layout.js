48de8e32c1707d2bb0f964442dd84ad79cfbacb2
mantaslv <mantasvolkas@gmail.com>
2023-04-13 17:07:29 +0100

    Added app bar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,31 +1,49 @@
 import { AddCircleOutlineOutlined, SubjectOutlined } from '@mui/icons-material'
-import { Drawer, Typography, List, ListItemIcon, ListItemText, ListItemButton } from '@mui/material'
+import { Drawer, Typography, List, ListItemIcon, ListItemText, ListItemButton, AppBar, Toolbar } from '@mui/material'
 import { makeStyles } from '@mui/styles'
 import React from 'react'
 import { useHistory, useLocation } from 'react-router-dom/cjs/react-router-dom.min'
+import { format } from 'date-fns'
 
 const drawerWidth = 240
 
-const useStyles = makeStyles(() => ({
-    page: {
-        background: '#f9f9f9',
-        width: '100%'
-    },
-    drawer: {
-        width: drawerWidth
-    },
-    drawerPaper: {
-        width: drawerWidth
-    },
-    root: {
-        display: 'flex'
-    },
-    active: {
-        '&.Mui-selected': {
-            background: '#f4f4f4'
+const useStyles = makeStyles((theme) => {
+    return {
+        page: {
+            background: '#f9f9f9',
+            width: '100%',
+            padding: theme.spacing(3)
+        },
+        drawer: {
+            width: drawerWidth
+        },
+        drawerPaper: {
+            width: drawerWidth
+        },
+        root: {
+            display: 'flex'
+        },
+        active: {
+            '&.Mui-selected': {
+                background: '#f4f4f4'
+            }
+        },
+        title: {
+            padding: theme.spacing(2)
+        },
+        appBar: {
+            '& .MuiToolbar-root': {
+                //width: `calc(100% - ${drawerWidth}px)`, // Set the width of the Toolbar component
+                marginLeft: drawerWidth,
+                background: '#fff'
+            }
+        },
+        toolbar: theme.mixins.toolbar,
+        date: {
+            flexGrow: 1
         }
-    }    
-}))
+    }
+})
 
 export default function Layout({ children }) {
     const classes = useStyles()
@@ -48,6 +66,19 @@ export default function Layout({ children }) {
     return (
         <div className={classes.root}>
             {/* app bar */}
+            <AppBar
+                className={classes.appBar}
+                elevation={0}
+            >
+                <Toolbar>
+                    <Typography className={classes.date}>
+                        Today is the {format(new Date(), 'do MMMM Y')}
+                    </Typography>
+                    <Typography>
+                        Mantas
+                    </Typography>
+                </Toolbar>
+            </AppBar>
 
             {/* side drawer */}
             <Drawer className={classes.drawer} 
@@ -56,7 +87,7 @@ export default function Layout({ children }) {
                 classes={{ paper: classes.drawerPaper }}
             >
                 <div>
-                    <Typography variant='h5'>
+                    <Typography variant='h5' className={classes.title}>
                         My Notes
                     </Typography>
                 </div>
@@ -68,7 +99,6 @@ export default function Layout({ children }) {
                             onClick={() => { history.push(item.path) }}
                             selected={location.pathname === item.path}
                         >
-                            {console.log(item.path)}
                             <ListItemIcon>{item.icon}</ListItemIcon>
                             <ListItemText primary={item.text} />
                         </ListItemButton>
@@ -77,6 +107,7 @@ export default function Layout({ children }) {
             </Drawer>
 
             <div className={classes.page}>
+                <div className={classes.toolbar}></div>
                 {children}
             </div>
         </div>
--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import { Card, CardHeader, CardContent, IconButton, Typography } from '@mui/material'
 import { DeleteOutlined } from '@mui/icons-material'
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles({
+    test: {
+        border: (note) => {
+            if (note.category === 'work') {
+                return '1px solid red'
+            }
+        }
+    }
+})
 
 export default function NoteCard({ note, handleDelete }) {
+    const classes = useStyles(note)
+
   return (
     <div>
-        <Card elevation={1}>
+        <Card elevation={1} className={classes.test}>
             <CardHeader 
                 action={
                     <IconButton onClick={() => handleDelete(note.id)}>
