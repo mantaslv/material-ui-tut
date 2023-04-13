629a363989e9eda01d35359ade19cb4e1cfcda13
mantaslv <mantasvolkas@gmail.com>
2023-04-13 10:10:11 +0100

    Added side drawer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,22 @@
+import { Drawer, Typography } from '@mui/material'
 import { makeStyles } from '@mui/styles'
 import React from 'react'
+
+const drawerWidth = 240
 
 const useStyles = makeStyles({
     page: {
         background: '#f9f9f9',
         width: '100%'
+    },
+    drawer: {
+        width: drawerWidth
+    },
+    drawerPaper: {
+        width: drawerWidth
+    },
+    root: {
+        display: 'flex'
     }
 })
 
@@ -12,10 +24,21 @@ export default function Layout({ children }) {
     const classes = useStyles()
 
     return (
-        <div>
+        <div className={classes.root}>
             {/* app bar */}
 
             {/* side drawer */}
+            <Drawer className={classes.drawer} 
+                variant='permanent' 
+                anchor='left'
+                classes={{ paper: classes.drawerPaper }}
+            >
+                <div>
+                    <Typography variant='h5'>
+                        My Notes
+                    </Typography>
+                </div>
+            </Drawer>
 
             <div className={classes.page}>
                 {children}
